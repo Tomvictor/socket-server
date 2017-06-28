f34cafd1f3baa5f4538840b811c92deeab3631ff
Tom Victor <vjtomvictor@icloud.com>
2017-06-28 19:53:52 +0530

    data parsed multiple encodings

--- echo.js
+++ echo.js
@@ -1,14 +1,36 @@
 var net = require('net');
 
 var server = net.createServer(function(socket) {
+	//listening event is not working
 	socket.on('listening',() => {
 		console.log('listening for connections');
 	    });
 	console.log("client connected"); //message on new connection
-	console.log(socket.address());
+	console.log(socket.address()); //This will display the client address
 	socket.write('Technorip Echo server\r\n'); //imediate message to client
+	//tring to read the data
+	socket.on('data',(data) => {
+		var chunck ;
+		chunck = data;
+		console.log("Data chunck:");
+		console.log(chunck);
+		console.log("Data length:");
+		console.log(chunck.length);
+		console.log("Data in ascii");
+		console.log(chunck.toString('ascii'));
+		console.log("Data in utf8");
+		console.log(chunck.toString('utf8'));
+		console.log("Data in utf16le");
+		console.log(chunck.toString('utf16le'));
+		console.log("Data in binary");
+		console.log(chunck.toString('binary'));
+		console.log("Data in hex");
+		console.log(chunck.toString('hex'));
+		console.log(data.length);
+		console.log(data);
+	    });
 
-	socket.pipe(socket);	
+	//socket.pipe(socket);	
 
 	//message on disconnecting client
 	socket.on('end', () => {
