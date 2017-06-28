e0e8cd9504451a2fc445b17f73b8efe4c1b72644
Tom Victor <vjtomvictor@icloud.com>
2017-06-28 19:06:11 +0530

    changes

--- echo.js
+++ echo.js
@@ -1,8 +1,19 @@
 var net = require('net');
 
 var server = net.createServer(function(socket) {
-	socket.write('Technorip Echo server\r\n');
-	socket.pipe(socket);
+	socket.on('listening',() => {
+		console.log('listening for connections');
+	    });
+	console.log("client connected"); //message on new connection
+	consile.log(socket.address());
+	socket.write('Technorip Echo server\r\n'); //imediate message to client
+
+	socket.pipe(socket);	
+
+	//message on disconnecting client
+	socket.on('end', () => {
+		console.log('client disconnected');
+	    });
     });
 
 server.listen(8010, '0.0');
